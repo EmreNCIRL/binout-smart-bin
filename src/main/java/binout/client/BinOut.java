b4899252b6a107ba6866a14fcf01e47389f46295
Emre Ketme <emurekun@macbookpro.home>
2025-07-10 21:21:50 +0100

    Add ServiceRegistry java and proto files

---
+++ src/main/java/binout/client/BinOut.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package binout.client;
+
+/**
+ *
+ * @author emurekun
+ */
+public class BinOut {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
